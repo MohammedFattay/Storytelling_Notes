8f74370213b30f03cdc184d48a0ab1639646d2c0
Mohammed_Fattay <teravisionsai@gmail.com>
2026-08-07 17:37:22 +0300

    fix: البحث يستجيب فورًا بإشعار تحميل، ولا بياض في وضع القراءة (ناتج مشفَّر)

--- postscript-4c8a6820.js
+++ postscript-4c8a6820.js
@@ -1,7 +1,7 @@
 await Promise.all([
   import("./static/scripts/script-0-f7c52b6a.js"),
   import("./static/scripts/script-1-0e41d61b.js"),
-  import("./static/scripts/script-2-ec880d15.js"),
+  import("./static/scripts/script-2-ce30d286.js"),
   import("./static/scripts/script-3-1f9394e6.js"),
   import("./static/scripts/script-4-ec85659d.js"),
   import("./static/scripts/script-5-5ef560a8.js"),
