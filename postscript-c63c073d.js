b5db172ff20c88e73a19b7bef6cef12b9acfecfb
Mohammed_Fattay <teravisionsai@gmail.com>
2026-08-07 10:46:08 +0300

    sync: تحديث المحتوى من الفولت (ناتج مشفَّر)

---
+++ postscript-c63c073d.js
@@ -0,0 +1,12 @@
+await Promise.all([
+  import("./static/scripts/script-0-f7c52b6a.js"),
+  import("./static/scripts/script-1-0e41d61b.js"),
+  import("./static/scripts/script-2-ec880d15.js"),
+  import("./static/scripts/script-3-1f9394e6.js"),
+  import("./static/scripts/script-4-ec85659d.js"),
+  import("./static/scripts/script-5-5ef560a8.js"),
+  import("./static/scripts/script-6-632b2822.js"),
+  import("./static/scripts/script-7-296b30c3.js"),
+  import("./static/scripts/script-8-be15df42.js")
+]);
+await import("./static/scripts/script-9-d93af9ba.js");
